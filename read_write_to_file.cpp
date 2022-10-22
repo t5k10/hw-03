b81295e6111354c5ba80ac3c657e6fb3eee81e97
David <vatrushkinblues@yandex.ru>
2022-10-22 21:51:38 +0300

    Homework # 3

--- read_write_to_file.cpp
+++ read_write_to_file.cpp
@@ -3,24 +3,41 @@
 #include <climits>
 
 const std::string high_scores_filename = "high_scores.txt";
-     
-     // Write 
+      
 bool write_to_file(std::string user_name, int attempts_count) {
-    std::ofstream out_file{high_scores_filename, std::ios_base::app};
-    if (!out_file.is_open()) {
-         std::cout << "Failed to open file for write: " << high_scores_filename << "!" << std::endl;
-         return false;
+    std::fstream file{high_scores_filename, std::ios_base::out | std::ios_base::in};
+    if (!file.is_open()) {
+        file.clear();
+        file.open(high_scores_filename, std::ios_base::app);
     }
-    out_file << user_name << ' ';
-    out_file << attempts_count;
-    out_file << std::endl;
-    out_file.close();
-    return true;
+    std::string name;
+    int score;
+    bool exist_name = false;
+    while (true) { 
+        int pos_in_file = file.tellp();
+        file >> name;
+        file >> score;
+        file.ignore();
+        if (file.fail()) break;
+        if (user_name == name) {
+            if (attempts_count < score) {
+                exist_name = true;
+                file.seekp(pos_in_file);
+                file << name << ' ' << attempts_count << std::endl; 
+            } 
+            else exist_name = true; 
+        }
+    }
+    if (!exist_name) {
+        file.clear();
+        file << user_name << ' ' << attempts_count << std::endl;
+    }
+    file.close();
+    return true;    
 }
 
-     // Read 
 bool read_from_file() {
-    std::ifstream in_file{high_scores_filename};
+    std::ifstream in_file{high_scores_filename, std::ios_base::in};
     if (!in_file.is_open()) {
         std::cout << "Failed to open file for read: " << high_scores_filename << "!" << std::endl;
         return false;
